dfea418cec23389f63afe70bd1bda1b81d163503
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-22 21:59:14 -0600

    Create P1-hola mundo.py

---
+++ Codigo/P1-hola mundo.py
@@ -0,0 +1,11 @@
+# Codigo "Hola mundo"
+
+# Definicion de la funcion 
+ ```
+def imprimir_saludo():
+    print("¡Hola, mundo!")
+  if __name__ == "__main__":
+    imprimir_saludo()
+ ```
+#Llamado a la funcion 
+ imprimir_saludo()

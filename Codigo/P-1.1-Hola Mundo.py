0dcab83974185269eb190cf6994b1c36222027c9
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-26 17:00:14 -0600

    Create P-1.1-Hola Mundo.py

---
+++ Codigo/P-1.1-Hola Mundo.py
@@ -0,0 +1,7 @@
+# Programa 1: Programa que imprime un mensaje de texto en pantalla 
+# Fecha de elaboracion: 2024/10/14 
+# Elaborado por: Noemi Lazarin Rosas 
+
+```
+print("Hola, mi nombre es Noemi") 
+```

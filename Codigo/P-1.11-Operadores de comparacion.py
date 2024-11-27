80a2bc50712f4b0368cab7023d55e637cedd3c78
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-27 16:53:10 -0600

    Create P-1.11-Operadores de comparacion.py

---
+++ Codigo/P-1.11-Operadores de comparacion.py
@@ -0,0 +1,39 @@
+#Programa 11: programa para analizar los operadores de comparacion ==,!=,>=,<=,>,<
+#Fecha: 18/10/2024
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+print("Operador IGUAL (==) ")
+print(5==7) # True
+print(5==5)# False
+print(5.0==5)# True 
+print("\n")
+
+print("Operador DIFERENTE O NO IGUAL (!=)") 
+print(5 != 5) # False 
+print(10 != 7) # True 
+print (5.0 != 5) # False 
+print("\n")
+
+print("Operador MAYOR QUE (>)")
+print(10 > 5) #True
+print(5 > 10) #False
+print("\n")
+
+print("Operador MENOR QUE (<)")
+print( 5 < 10) #True 
+print(10 < 5 ) # False 
+print("\n")
+
+print ("Operador MENOR O IGUAL QUE (<=)")
+print(5 <= 5) #True 
+print(10 <= 7 ) #False 
+print(3 <= 5) #True 
+print("\n")
+
+print("Operador MAYOR O IGUAL QUE (>=)")
+print(5 >= 5) #True
+print(10 >= 7) #False 
+print(3 >= 5) #True 
+print("\n")
+```

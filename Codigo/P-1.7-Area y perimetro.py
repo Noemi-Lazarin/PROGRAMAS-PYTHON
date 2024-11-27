f3aee71fe4a57021c56a6fc95a5d9ef08d413448
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-27 16:47:59 -0600

    Create P-1.7-Area y perimetro.py

---
+++ Codigo/P-1.7-Area y perimetro.py
@@ -0,0 +1,12 @@
+#Programa 7: calcula el area y el perimetro de un circulo ingresando el valor del radio por el usuario 
+#Fecha: 2024/10/15
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+radio= float(input("ingresa el valor del radio:")) 
+areaCirculo = 3.1416 * radio ** 2
+perimetroCirculo = 3.1416 * 2 * radio 
+
+print("El valor del area es: " + str(areaCirculo))
+print("El valor del perimetro es:" + str(perimetroCirculo))
+```

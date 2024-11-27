7551a98759c52f620b3200130746fbe707840553
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-27 16:45:20 -0600

    Create P-1.5.py

---
+++ Codigo/P-1.5.py
@@ -0,0 +1,17 @@
+#Programa 5. 
+#Fecha: 2024/10/14
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+num1 = int(input("ingresa el primer numero:"))
+num2 = int(input("ingresa el segundo numero:"))
+suma = num1 + num2
+resta = num1 - num2 
+multiplicacion = num1 * num2 
+division = num1 / num2 
+
+print("El valor de la suma:" + str(suma))
+print("El valor de la resta:" + str(resta)) 
+print("El valor de la multiplicacion:" + str(multiplicacion))
+print("El valor de la division:" + str(division)) 
+```

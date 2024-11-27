803d8975076731e9686a4a00688cb4ef447e005b
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-27 16:49:22 -0600

    Create P-1.8-Numero de minutos ,horas y meses.py

---
+++ Codigo/P-1.8-Numero de minutos ,horas y meses.py
@@ -0,0 +1,15 @@
+#Programa 8: Realice un programa que calcule el numero de minutos horas y meses, dando el numero de dias por el usuario 
+#Fecha: 2024/10/16
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+dias = int(input("Ingresa el numero de dias: "))
+# Calculos
+horas = dias * 24
+minutos = dias * 24 * 60 
+meses = dias / 30 
+#Mostrar los resultados 
+print ("El numero de minutos es: ", (minutos))
+print ("El numero de horas es:", (horas))
+print ("El numero de meses es: ", (meses))
+```

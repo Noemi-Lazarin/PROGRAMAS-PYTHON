8c0084d8eea82344e56c4f8db2bf109c1ff8a3ca
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-27 16:54:14 -0600

    Create P-1.12-Compuertas logicas.py

---
+++ Codigo/P-1.12-Compuertas logicas.py
@@ -0,0 +1,18 @@
+# Programa 12: programa para probar las compuertas logicas AND, OR Y NOT 
+#Fecha: 18/10/2024
+#Elaborador por: Noemi Lazarin Rosas 
+
+```
+print("compuerta or")
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+
+print("\n Compuerta AND")
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and False )
+```

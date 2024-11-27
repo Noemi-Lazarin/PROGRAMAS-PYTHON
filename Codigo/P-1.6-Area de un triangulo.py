66d3602b017dd33c1ca2d37f6211707d583d2b04
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-27 16:46:42 -0600

    Create P-1.6-Area de un triangulo.py

---
+++ Codigo/P-1.6-Area de un triangulo.py
@@ -0,0 +1,11 @@
+#Programa 6: Calcula el area de un triangulo dados la base y la altura al usuario 
+#Fecha: 2024/10/15
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+base = float(input("ingresa el valor de la base: "))
+altura=float(input("ingresa el valor de la altura: "))
+area= base + altura / 2 
+
+print("El area del triangulo es:" + str(area))
+```

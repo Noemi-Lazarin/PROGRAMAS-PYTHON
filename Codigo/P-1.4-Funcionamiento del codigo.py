10586f60717efdf95f1079fd9093fbd78f7a22c9
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-11-27 16:39:03 -0600

    Create P-1.4-Funcionamiento del codigo.py

---
+++ Codigo/P-1.4-Funcionamiento del codigo.py
@@ -0,0 +1,11 @@
+#Programa 4: Probar el funcionamiento del siguiente codigo 
+#Fecha: 2024/10/14 
+#Elaborado por noemi lazarin rosas 
+
+```
+print(1+2)
+print(1-2)
+print(1*2)
+print("concatenando cadenas" + "esta bien" )
+#concatenando cadenas - esta bien esta mal
+```

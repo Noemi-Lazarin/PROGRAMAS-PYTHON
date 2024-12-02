14580de50ddbbf386f20623eb59f45a16be762b0
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-02 17:43:34 -0600

    Create B2-P-1.2.py

---
+++ Codigo/B2-P-1.2.py
@@ -0,0 +1,16 @@
+#Programa 2: Programa si chorchis o choto van a la fiesta, entonces NO hay fiesta.
+#Fecha de elaboracion: Noemi Lazarin Rosas 
+#Elaborado por: Noemi Lazarin Rosas
+
+#|A|B||NOT (A OR B)|
+#|0|0|1
+#|0|1|0
+#|1|0|0
+#|1|1|0
+
+```
+print(not (False or False)) #Si hay fiesta
+print(not (False or True)) #No hay fiestaa
+print(not (True or False)) #No hay fiesta 
+print(not (True or True)) #No hay fiesta 
+```

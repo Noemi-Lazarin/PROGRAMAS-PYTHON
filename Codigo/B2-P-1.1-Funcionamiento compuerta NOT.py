93085cd0f45085c5ea5275e170729330b647ff1e
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-02 17:39:38 -0600

    Create B2-P-1.1-Funcionamiento compuerta NOT.py

---
+++ Codigo/B2-P-1.1-Funcionamiento compuerta NOT.py
@@ -0,0 +1,9 @@
+#Programa 1:Demuestre el funcionamiento compuerta NOT 
+#Fecha de elaboracion: 22/10/2024 
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+print("Compuerta NOT" )
+print(not True) #Imprime FALSE
+print(not False) #Imprime TRUE
+```

f021ed6854ed3a09e387ca7057ea117196a58241
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-02 17:45:00 -0600

    Create B2-P-1.3-Concatenacion.py

---
+++ Codigo/B2-P-1.3-Concatenacion.py
@@ -0,0 +1,7 @@
+#Prgorama 3: Comprueba el funcionamiento de la adicion o CONCATENACION de varias cadenas de texto (string)
+#Fecha de elaboracion: 22/10/2024
+#Elaborado por: Noemi Lazarin Rosas
+
+```
+print("Mi" + "nombre" + "es" + "Edinguer")
+```

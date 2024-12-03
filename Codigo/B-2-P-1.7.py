bd0f0ba805bd9e67e139ce14bf409ae872d00f0b
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-02 18:34:03 -0600

    Create B-2-P-1.7.py

---
+++ Codigo/B-2-P-1.7.py
@@ -0,0 +1,12 @@
+#Programa 7: programa que solicite la edad e indique que si puede entrar a un bar 
+#Fecha de elaboracion: 24/10/2024
+#Elaborado por: Noemi Lazarin Rosas
+
+```
+edad=input("ingresa tu edad:") 
+if edad >= 18:
+   print("Puede ingresar al bar") 
+else: 
+   print("vete a tu casa")
+print("fin del programa")
+```

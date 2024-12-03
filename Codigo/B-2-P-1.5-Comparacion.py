227955469f7deecd0f3e6c5cd0dc99f54adb6565
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-02 18:20:59 -0600

    Create B-2-P-1.5-Comparacion.py

---
+++ Codigo/B-2-P-1.5-Comparacion.py
@@ -0,0 +1,34 @@
+#Programa 5: Comparacion
+#Fecha de elaboracion: 02/12/2024
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+#Comparador
+print("perro" == "perro") #true
+print("perro" != "gato") #true
+print("aguascalientes" < "zacatecas") #true
+print("aire" >= "agua") #true 
+
+# Membership 
+# operador in 
+#Revisar si la primer cadena esta en la segunda 
+print("house" in "boathouse") #True
+print("bien" in "bienvenidos") #True 
+print("y" not in "ejes") #True
+print("je" not in "ejes") #False
+
+# Practica 5:
+#Fecha de elaboracion: 23/10/2024
+# Elaborado por: Noemi Lazarin Rosas 
+
+# indexing (indexado)
+mi_nombre = "choto chorchis"
+print(mi_nombre [3])
+print(mi_nombre [12] ) 
+
+# Subscripting multiple 
+# characters (sustraccion)
+
+mi_nombre = "Jose luis"
+print(mi_nombre [2,6])
+```

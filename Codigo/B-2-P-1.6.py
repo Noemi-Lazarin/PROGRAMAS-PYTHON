8bcf1b93572da25d9ebf744d943a926ad57974a6
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-02 18:24:55 -0600

    Create B-2-P-1.6.py

---
+++ Codigo/B-2-P-1.6.py
@@ -0,0 +1,11 @@
+#Programa 6: 
+#Fecha de elaboracion: 24/10/2024
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+#Negative subscript indices
+print("word" [-1])
+print("word" [-1:1])
+print("word" [-3:])
+print("word" [:3])
+```

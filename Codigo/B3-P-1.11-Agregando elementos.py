057c4686cb25fdc29b683f9fedfa85a6deb66aaa
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:13:22 -0600

    Create B3-P-1.11-Agregando elementos.py

---
+++ Codigo/B3-P-1.11-Agregando elementos.py
@@ -0,0 +1,11 @@
+#Programa 11: agregando elementos a una lista 
+#Fecha de elaboracion : 31/10/24
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+colores=["Rojo","Azul"] 
+print(colores)
+colores.append("Verde")
+print(colores)
+#print ("colores" + "amarrillo") #error 
+```

71da969afc76cbc11e4316b13275faad32e0221b
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:24:30 -0600

    Create B3-P-1.12-Metodo POP en una lista.py

---
+++ Codigo/B3-P-1.12-Metodo POP en una lista.py
@@ -0,0 +1,18 @@
+#Programa 12: Programa utilizando el metodo pop() en una lista 
+# Fecha de elaboracion: 31/10/2024
+# Elaborado por: Noemi Lazarin Rosas
+
+```
+# Creamos una lista 
+mi_lista = ["Rojo", "Verde", "Morado", "Azul", "Naranja"]
+
+# Usamos pop para eliminar y devolver el último elemento
+ultimo_elemento = mi_lista.pop()
+print("Elemento eliminado:", ultimo_elemento)
+print("Lista después de usar pop:", mi_lista)
+
+# Usamos pop para eliminar y devolver el elemento en la posición 1
+elemento_en_posicion_1 = mi_lista.pop(1)
+print("Elemento eliminado en la posición 1:", elemento_en_posicion_1)
+print("Lista después de usar pop en la posición 1:", mi_lista)
+```

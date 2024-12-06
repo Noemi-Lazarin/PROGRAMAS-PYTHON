8a029b2c37f14bbba5c73a739adaf09775aba663
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-06 17:31:56 -0600

    Create B4-P-1.1-Metodo POP.py

---
+++ Codigo/B4-P-1.1-Metodo POP.py
@@ -0,0 +1,16 @@
+#Programa 1: Programa utilizando el metodo pop() en una lista 
+#Fecha de elaboracion: 04/11/2024
+#Elaborado por: Noemi Lazarin Rosas 
+
+#Programa 1
+lista_num = [10,30,40,20,35,80]
+print(lista_num[0])
+print(lista_num[1])
+print(lista_num[2])
+print(lista_num[3])
+print(lista_num[4])
+print(lista_num[5])
+
+#Haciedno lo mismo pero con ciclo "for"
+for i in lista_num:
+    print(i) 

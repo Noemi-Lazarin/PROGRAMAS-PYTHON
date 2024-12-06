07fdd5f88b59f53e549cc15108b1c3b0e0d032cb
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:09:33 -0600

    Create B3-P-1.8-Operaciones con listas 1.py

---
+++ Codigo/B3-P-1.8-Operaciones con listas 1.py
@@ -0,0 +1,18 @@
+#Programa 8: Operaciones con listas 1 
+#Fecha de elaboracion: 30/10/2024
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+print("Indexing")
+numeros = [10,20,30]
+print(numeros[2])
+print(numeros[0])
+print(numeros[1])
+print(numeros[-1])
+
+print("\nSubscripting")
+nombres = ["Chorchis","Choto","Emiliano","Pepe el toro"]
+print(nombres[1:]) #Imprime ("Choto","Emiliano","Pepe el toro")
+print(nombres[1:5]) #Imprime ("Choto","")
+print(nombres[-2:]) #Imprime ("Emiliano","Pepe el toro")
+```

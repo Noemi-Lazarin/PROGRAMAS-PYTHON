b2acfc1b703d4615a996437b2de848c074cbcce7
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:02:32 -0600

    Create B3-P-1.3-Identificacion de tipos de datos.py

---
+++ Codigo/B3-P-1.3-Identificacion de tipos de datos.py
@@ -0,0 +1,14 @@
+# Programa 3: identificacion de tipos de datos str, int y flooat 
+# Fecha de elaboracion: 28/11/2024
+# Elaborado por: Noemi Lazarin Rosas 
+
+```
+variable = input("Ingresa tu edad: ")
+print(type(variable))
+
+variable = int(variable)  #Convierte la variable a tipo entero 
+print(type(variable))
+
+variable = float(variable) #convierto la variable a tipo flotante 
+print(type(variable))
+```

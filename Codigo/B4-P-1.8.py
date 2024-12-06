e3964590a2f57bcb6d7f398abe764011db5ad965
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-06 17:39:56 -0600

    Create B4-P-1.8.py

---
+++ Codigo/B4-P-1.8.py
@@ -0,0 +1,21 @@
+#Programa 8: 
+#Fecha de elaboracion: 07/11/2024
+#Elaborado por: Noemi Lazarin Rosas
+
+"""Las estructuras while se ejecutan mientras las condiciones que se encuentra sea verdadera."""
+#Ejemplo 1 
+# Imprime los numeros del 1 al 10 
+i = 1 # Inicializacion de la variable control 
+while i <= 10: #Condicion de parada 
+    print(i)
+    1+= 1 #Equivalente a i = + 1
+#Sintaxis
+#While <condicion>:
+#   <cuerpo de While>
+
+#Ejemplo 2
+#Imprimir los numeros del 10 al 1
+i = 10 
+while i >= 1:
+    print(i)
+    i -= 1

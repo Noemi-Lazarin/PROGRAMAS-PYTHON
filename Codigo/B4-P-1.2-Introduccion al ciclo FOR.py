1da612eb28d47c9eb51cc8965e322b76523a38d5
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-06 17:32:43 -0600

    Create B4-P-1.2-Introduccion al ciclo FOR.py

---
+++ Codigo/B4-P-1.2-Introduccion al ciclo FOR.py
@@ -0,0 +1,11 @@
+#Programa 2: Introduccion al ciclo "for"
+#Fecha de elaboracion: 04/11/2024
+#Elaborado por: Noemi Lazarin Rosas 
+
+nombres = ["Luis","Chuy","Mauricio"]
+for nombre in nombres: 
+    print("El nombre es:",nombre) 
+    
+frutas = ["Manzana","Piña","Platano"]
+for fruta in frutas:
+    print("La fruta es:", fruta)

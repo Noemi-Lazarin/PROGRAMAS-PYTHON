f49140357764adead49637b9a45116a8db78a6d4
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:06:41 -0600

    Create B3-P-1.6-Listas.py

---
+++ Codigo/B3-P-1.6-Listas.py
@@ -0,0 +1,17 @@
+#Programa 6: Listas 
+# Fecha de elaboracion: 30/10/24 
+# Elaborado por: Noemi Lazarin Rosas 
+
+```
+print("Listas de enteros") 
+print([40,20,35])
+
+print("\nLista de cadenas de texto")
+print(["Manzana","Piña","Naranja"])
+
+print("\nLista de boleanos") 
+print([False, True, True])
+
+print("Lista vacia") 
+print([])
+```

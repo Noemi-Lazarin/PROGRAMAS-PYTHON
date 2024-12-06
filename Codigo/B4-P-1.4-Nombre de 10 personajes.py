3d3af7190260b708851b9c8a571b587c3d3682d0
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-06 17:34:14 -0600

    Create B4-P-1.4-Nombre de 10 personajes.py

---
+++ Codigo/B4-P-1.4-Nombre de 10 personajes.py
@@ -0,0 +1,12 @@
+#Programa 4: programa que imprima el nombre de tus 10 personajes favoritos de...
+#Fecha de elaboracion: 05/11/2024
+#Elaborado por: NOEMI Lazarin Rosas 
+
+personajes = ["Bob esponja","Patricio","Calamardo","Arenita","Don cangrejo","Perlita","Planton","Sireno man","Chico persebe","Gary"]
+
+#Imprimir cada personaje en la lista 
+print("Los 10 personajes favoritos de la caricatura de bob esponja son:")
+for personaje in personajes:
+    print(personajes)
+    
+print("Gracias por ver los 10 personajes favoritos de Noemi Lazarin")

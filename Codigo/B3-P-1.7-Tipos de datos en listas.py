d0eeb63e3aa11167b1f2ada6c70c6119e86a4a8e
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:08:14 -0600

    Create B3-P-1.7-Tipos de datos en listas.py

---
+++ Codigo/B3-P-1.7-Tipos de datos en listas.py
@@ -0,0 +1,23 @@
+#Programa 7: Tipos de datos en las listas (Crear un programa con 3 listas con elementos)
+#Fecha de elaboracion: 30/10/2024
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+cadenas = ["Juan","Pedro","Chuy"]
+print(cadenas) #Muestra la lista completa 
+print(cadenas[1]) #Imprime "pedro"
+print(type(cadenas)) #Imprime el tipo "list"
+print(type(cadenas[1])) #Imprime el tipo "str"
+
+enteros = [40,20,35]
+print(enteros)
+print(enteros[1])
+print(type(enteros))
+print(type(enteros[1]))
+
+booleanos = [False,True,True]
+print(booleanos)
+print(booleanos[1])
+print(type(booleanos))
+print(type(booleanos[1])) 
+```

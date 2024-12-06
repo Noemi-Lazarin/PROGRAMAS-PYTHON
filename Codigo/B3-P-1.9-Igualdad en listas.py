2bf99d94eadd010488fa69b0e6f7274cb5040f58
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:10:58 -0600

    Create B3-P-1.9-Igualdad en listas.py

---
+++ Codigo/B3-P-1.9-Igualdad en listas.py
@@ -0,0 +1,19 @@
+#Programa 9: Igualdad en listas. compare puntos con todas las demas listas para comprobar si son diferentes o igaules
+#Fecha de elaboracion: 31/10/24
+#Elaborado por: Noemi Lazarin Rosas 
+
+```
+puntos = [10,30,20]
+
+puntos_2 = [10,30,20]
+ordenados = [10,20,30]
+puntos_texto = ["10","20","30"]
+
+print(puntos==puntos_2) #True
+print(puntos==ordenados) #False
+print(puntos==puntos_texto) #False 
+
+print(puntos!=puntos_2) #False
+print(puntos!=ordenados) #True
+print(puntos!=puntos_texto) #True
+```

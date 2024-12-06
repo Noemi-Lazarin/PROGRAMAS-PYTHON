492edbf7e831170ca119a227e475f065f5da2605
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-06 17:44:18 -0600

    Create B4-P-1.13.py

---
+++ Codigo/B4-P-1.13.py
@@ -0,0 +1,13 @@
+#Programa 13
+# Fecha de elaboracion: 12/11/24
+#Elaborado por: Noemi Lazarin Rosas 
+
+
+# Cadenas de texto formateadas 
+nombre = "Noemi"
+edad = 18
+calificacion = 93.8
+print(f"Nombre: {nombre}, edad: {edad}, promedio: {calificacion}") 
+
+#Utilizando el caracter %
+print("Nombre: %s, edad %d, promedio:%2f %(nombre,edad,calificacion)")

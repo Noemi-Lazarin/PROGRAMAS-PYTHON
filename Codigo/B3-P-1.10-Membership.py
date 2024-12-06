0b73395006a56234a85cb51c946edd1a283e2df4
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-05 18:12:03 -0600

    Create B3-P-1.10-Membership.py

---
+++ Codigo/B3-P-1.10-Membership.py
@@ -0,0 +1,12 @@
+# Programa 10: Membership 
+# Fecha de elaboracion: 31/10/2024
+# Elaborado por: Noemi Lazarin Rosas
+
+```
+nombres=["Choto","Emiliano","Luis"] 
+
+print("Luis" in nombres) #True
+print("Emi" in nombres) #False
+print("Javier" in nombres) #False
+print("Jose" not in nombres) #True 
+```

36723a75f9182464f4f3b0c0888d7e24fdbab89a
Noemi-Lazarin <noemi.123lazarin@gmail.com>
2024-12-06 17:41:17 -0600

    Create B4-P-1.10.py

---
+++ Codigo/B4-P-1.10.py
@@ -0,0 +1,12 @@
+"""Programa para demostrar el funcionamiento de los metodos upper() y lower ()"""
+
+#Definimos una cadena de texto 
+cadena = "Python es un lenguaje de programacion"
+print("\n",cadena)
+
+cadena_mayusculas = cadena.upper() #Convertimos la cadena a mayusculas 
+print("\n", cadena_mayusculas)
+
+cadena_minusculas = cadena.lower() #Convertimos la cadena a minusculas
+print("\n", cadena_minusculas)
+print("\nFin del programa")
